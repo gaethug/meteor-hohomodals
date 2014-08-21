27544a66319d6ab52f25073567febde843b88e72
gaethug <cbak@naver.com>
2014-08-21 09:41:52 +0900

    네임변경

--- modal_base.js
+++ modal_base.js
@@ -5,7 +5,7 @@
  * Time: 오전 9:59
  * To change this template use File | Settings | File Templates.
  */
-Template.modal_base.helpers({
+Template.hoho_modal.helpers({
   modalList: function() {
     return HoHoModals.collection.find().fetch();
   }
